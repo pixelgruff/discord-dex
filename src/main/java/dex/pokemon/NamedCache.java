60c3d1631fcb51fc61732a071fe34cea661eea7b
U-KINGLER\pixelgruff <pixelgruff@gmail.com>
2016-10-18 21:38:58 -0700

    Added retry logic to cache

--- src/main/java/dex/pokemon/NamedCache.java
+++ src/main/java/dex/pokemon/NamedCache.java
@@ -1,14 +1,21 @@
 package dex.pokemon;
 
+import com.github.rholder.retry.Retryer;
+import com.github.rholder.retry.RetryerBuilder;
+import com.github.rholder.retry.StopStrategies;
+import com.github.rholder.retry.WaitStrategies;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import com.google.common.collect.ImmutableMap;
 import me.sargunvohra.lib.pokekotlin.model.NamedApiResource;
 import me.sargunvohra.lib.pokekotlin.model.NamedApiResourceList;
+import org.apache.commons.lang3.Validate;
+import org.jetbrains.annotations.NotNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
@@ -21,6 +28,9 @@ import java.util.function.Function;
 public class NamedCache<T> {
     private static final Logger LOG = LoggerFactory.getLogger(NamedCache.class);
 
+    // Retryer object to retry troublesome API calls
+    private final Retryer<T> retryer_;
+
     // Function to provide the PokeAPI values by ID
     private final Function<Integer, T> valueProducer_;
     // Mapping of names to IDs
@@ -30,8 +40,8 @@ public class NamedCache<T> {
             .expireAfterAccess(24, TimeUnit.HOURS)
             .build(new CacheLoader<Integer, T>() {
                 @Override
-                public T load(Integer key) throws Exception {
-                    return valueProducer_.apply(key);
+                public T load(@NotNull Integer key) throws Exception {
+                    return retryer_.call(() -> valueProducer_.apply(key));
                 }
             });
 
@@ -39,8 +49,22 @@ public class NamedCache<T> {
     {
         valueProducer_ = valueProducer;
         idMap_ = idMap;
+
+        // TODO: Some kind of appconfig instead of defaults scattered everywhere
+        retryer_ = RetryerBuilder.<T>newBuilder()
+                .retryIfExceptionOfType(IOException.class)
+                .withWaitStrategy(WaitStrategies.exponentialWait(10, TimeUnit.SECONDS))
+                .withStopStrategy(StopStrategies.stopAfterDelay(60, TimeUnit.SECONDS))
+                .build();
     }
 
+    /**
+     * Construct a cache to provide access via resource name to ID-based API calls
+     * @param nameProducer  Batched producer of {@link NamedApiResourceList}
+     * @param valueProducer Producer of resource to be cached
+     * @param <T>           Type of resource to be cached
+     * @return  A {@link NamedCache} initialized with a mapping of resource names -> resource IDs
+     */
     public static <T> NamedCache<T> initializeCache(
             final BiFunction<Integer, Integer, NamedApiResourceList> nameProducer,
             final Function<Integer, T> valueProducer)
@@ -48,6 +72,8 @@ public class NamedCache<T> {
         // Populate the cache with Pokemon entries
         final ImmutableMap.Builder<String, Integer> idMapBuilder = ImmutableMap.builder();
 
+        // TODO: Potential bug if we have a network problem while initializing the cache
+        // Recommend finding a way to compose retry logic onto the client itself
         final PaginatedNamedResourceList resourceList = PaginatedNamedResourceList.withBatchedProducer(nameProducer);
         for (final NamedApiResource resource : resourceList) {
             // Translate to lowercase for easier matching
@@ -62,6 +88,7 @@ public class NamedCache<T> {
 
     public Optional<T> get(final String name)
     {
+        Validate.notNull(name, "Cannot get a resource from a null key!");
         try {
             // Translate to lowercase for successful matching
             final Integer id = idMap_.get(name.toLowerCase());
@@ -71,7 +98,8 @@ public class NamedCache<T> {
                 return Optional.of(cache_.get(id));
             }
         } catch (ExecutionException e) {
-            return Optional.empty();
+            LOG.error("Encountered exception while retrieving value for key {}!", name, e);
+            throw new RuntimeException(e);
         }
     }
 }
